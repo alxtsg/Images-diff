892afdb5e2688e96f4d9dcb979491e53f859c785
Alex Tsang <alextsang@live.com>
2018-02-04 13:21:23 +0800

    Major rewrite.
    Major rewrite in ECMAScript 2017.
    Adds new option for cropping the images before comparison.
    Bumping MAJOR version because of incompatible configuration file structure.

--- README.md
+++ README.md
@@ -2,46 +2,61 @@
 
 ## Description ##
 
-Compares images in a directory in sequential manner and report the differences.
+Compare images in batch and report the differences.
 
 ## Requirements ##
 
-* Node.js (`>=4.4.5`).
-* GraphicsMagick (`>=1.3.24`).
+* Node.js (`>=8.9.4`).
+* GraphicsMagick (`>=1.3.28`).
 
 ## Installation ##
 
 0. Install Node.js.
 1. Install GraphicsMagick.
-2. Start the application by `node index.js <images-directory>`, where `<images-directory>` is the path of directory containing images for comparisons.
+2. Update configuration file `config.json`.
+3. Start the application by `node index.js <images-directory>`, where `<images-directory>` is the path of directory containing images for comparisons.
 
 ## Usage ##
 
 The configuration file `config.json` controls the following:
 
-* `gmPath`: The path of GraphicsMagick executable `gm` on BSD UNIX or `gm.exe` on Windows.
+* `gmPath`: The absolute path of GraphicsMagick executable.
 * `differenceThreshold`: The maximum mean squared error (MSE) that can be tolerated for each comparison.
-* `abnormalImagesDirectoryName`: The directory name where abnormal images will be copied into. The directory will be created automatically under the directory which containing images for analysis. If it is not specified, abnormal images will not be copied.
+* `abnormalImagesDirectory`: The directory where abnormal images will be copied into. The directory will be created automatically under the directory which containing images for analysis. If it is not specified, abnormal images will not be copied.
+* `crop`: If set to `null`, the images will not be cropped before comparsion. To crop the images, specify the following:
+  *  `width`: Width of the cropped image.
+  *  `height`: Height of the cropped image.
+  *  `offsetX`: Offset of location from the left of the image.
+  *  `offsetY`: Offset of location from the top of the image.
 
 ## Examples ##
 
-Assuming the directory containing images for comparisons is `/path/to/images/directory`, execute:
+Assuming the directory containing images for comparisons is `/path/to/images`, execute:
 
-`node index.js /path/to/images/directory`
+`node index.js /path/to/images`
 
 You will get output similar to the following:
 
-> Start at 2015-09-06T16:05:22.282Z.
+> Start comparing images at 2018-02-03T20:40:12.179Z.
 
-> Completed at 2015-09-06T16:20:28.192Z.
+> Completed comparing images at 2018-02-03T20:58:50.500Z.
 
-> 20150518-005330.jpeg, 20150518-005335.jpeg: WARN, 0.0031047677
+> /path/to/images/20180201-002618.jpeg, /path/to/images/20180201-002623.jpeg, 0.1477564507: WARN.
 
-> (A lot of lines are omitted.)
+> /path/to/images/20180201-002644.jpeg, /path/to/images/20180201-002649.jpeg, 0.0002248027: OKAY.
 
-> 20150518-005405.jpeg, 20150518-005410.jpeg: OKAY, 0.0000980678
+> (A lot of similar lines...)
 
-The output above shows that `20150518-005335.jpeg` differs very much from `20150518-005330.jpeg` (results `WARN`); `20150518-005410.jpeg` looks similar to `20150518-005405.jpeg` (results `OKAY`).
+> Start copying images at 2018-02-03T20:58:51.255Z.
+
+> Completed copying images at 2018-02-03T20:58:51.452Z.
+
+The output above shows:
+
+* The timestamp of beginning and end of comparisons.
+* `20180201-002618.jpeg` differs very much from `20180201-002623.jpeg` (MSE is 0.1477564507, which is above the `differenceThreshold`), so it gives a `WARN` message.
+* `20180201-002644.jpeg` looks similar to `20180201-002649.jpeg` (MSE is 0.0002248027, which is below the `differenceThreshold`), so it gives a `OKAY` message.
+* The timestamp of beginning and end of copying of abnormal images.
 
 ## License ##
 
--- index.js
+++ index.js
@@ -8,297 +8,346 @@
 
 'use strict';
 
-const EventEmitter = require('events').EventEmitter;
+const childProcess = require('child_process');
 const fs = require('fs');
+const os = require('os');
 const path = require('path');
 const process = require('process');
-const util = require('util');
 
-const ImagesDiff = require('./images-diff.js');
-const Copier = require('./copier.js');
-
-const EVENT_ERROR_READ_CONFIG = 'EVENT_ERROR_READ_CONFIG';
-const EVENT_ERROR_PARSE_CONFIG = 'EVENT_ERROR_PARSE_CONFIG';
-const EVENT_ERROR_READ_IMAGES_DIR = 'EVENT_ERROR_READ_IMAGES_DIR';
-const EVENT_ERROR_NO_IMAGES = 'EVENT_ERROR_NO_IMAGES';
-const EVENT_ERROR_ONE_IMAGE = 'EVENT_ERROR_ONE_IMAGE';
-const EVENT_ERROR_IMAGEDIFF_ERROR = 'EVENT_ERROR_IMAGEDIFF_ERROR';
-const EVENT_ERROR_CREATE_ABNORMAL_IMAGES_DIR =
-  'EVENT_ERROR_CREATE_ABNORMAL_IMAGES_DIR';
-const EVENT_APPLICATION_READY = 'EVENT_APPLICATION_READY';
-const EVENT_DIFF_COMPLETE = 'EVENT_DIFF_COMPLETE';
-const EVENT_COPY_ABNORMAL_IMAGES_DONE = 'EVENT_COPY_ABNORMAL_IMAGES_DONE';
-
-// Configuration file path.
-const CONFIG_FILE_PATH = path.join(
+const CONFIG_FILE = path.join(
   __dirname,
   'config.json'
 );
 
-/**
- * Main application.
- *
- * @extends EventEmitter
- */
-class Application extends EventEmitter {
+const CROPPED_FILE_A = 'file-a';
+const CROPPED_FILE_B = 'file-b';
 
-  /**
-   * Constructor.
-   *
-   * @class
-   * @constructor
-   *
-   * @param {String} imagesDirectoryPath Path of directory containing images
-   *                                     for comparison.
-   */
-  constructor(imagesDirectoryPath) {
-    super();
-    this.imagesDiff = null;
-    this.differenceThreshold = null;
-    this.imagesDirectoryPath = imagesDirectoryPath;
-    this.abnormalImagesDirectoryName = null;
-    this.abnormalImages = [];
-  }
+const COMPARISON_METRIC = 'mse';
+const COMPARISON_RESULT_REGEX = /Total: (\d+\.?\d*)/m;
 
-  /**
-   * Builds file path.
-   *
-   * @param {String} fileName Filename.
-   *
-   * @return {String} Path of file.
-   */
-  buildImageFilePath(fileName) {
-    return path.join(
-      this.imagesDirectoryPath,
-      fileName
-    );
-  }
+function generateCropImageCommand(originFile, outputFile, cropOption) {
+  const width = cropOption.width;
+  const height = cropOption.height;
+  const offsetX = cropOption.offsetX;
+  const offsetY = cropOption.offsetY;
+  return [
+    'convert',
+    `"${originFile}"`,
+    '-crop',
+    `${width}x${height}+${offsetX}+${offsetY}`,
+    `"${outputFile}"`,
+    '\n'
+  ].join(' ');
+}
 
-  /**
-   * Builds directory path of abnormal images.
-   *
-   * @return {String} Directory path of abnormal images.
-   */
-  buildAbnormalImagesDirectoryPath() {
-    return path.join(
-      this.imagesDirectoryPath,
-      this.abnormalImagesDirectoryName
-    );
-  }
+function generateCompareImagesCommand(fileA, fileB, comparisonMetric) {
+  return [
+    'compare',
+    '-metric',
+    comparisonMetric,
+    `"${fileA}"`,
+    `"${fileB}"`,
+    '\n'
+  ].join(' ');
+}
 
-  /**
-   * Builds file path of an abnormal image.
-   *
-   * @param {String} fileName Filename.
-   *
-   * @return {String} Path of the abnormal image.
-   */
-  buildAbnormalImageFilePath(fileName) {
-    return path.join(
-      this.imagesDirectoryPath,
-      this.abnormalImagesDirectoryName,
-      fileName
-    );
-  }
-
-  /**
-   * Parses configuration files.
-   */
-  parseConfig() {
+async function readConfigFile(configFilePath) {
+  return new Promise((resolve, reject) => {
     fs.readFile(
-      CONFIG_FILE_PATH,
+      configFilePath,
       {
         encoding: 'utf8'
       },
-      (readConfigError, data) => {
-        if (readConfigError !== null) {
-          this.emit(EVENT_ERROR_READ_CONFIG, readConfigError);
+      (readError, data) => {
+        if (readError !== null) {
+          console.error('Cannot read configuration file.');
+          reject(readError);
           return;
         }
         try {
           const config = JSON.parse(data);
-          this.imagesDiff = new ImagesDiff({
-            gmPath: config.gmPath
-          });
-          this.differenceThreshold = parseFloat(config.differenceThreshold);
-          if ((config.abnormalImagesDirectoryName !== undefined) &&
-            (config.abnormalImagesDirectoryName !== null)) {
-            this.abnormalImagesDirectoryName =
-              config.abnormalImagesDirectoryName;
-          }
-          this.emit(EVENT_APPLICATION_READY);
-        } catch (parseConfigError) {
-          this.emit(EVENT_ERROR_PARSE_CONFIG, parseConfigError);
+          resolve(config);
+        } catch (parseError) {
+          console.error('Cannot parse configuration file.');
+          reject(parseError);
         }
       }
     );
-  }
+  });
+}
 
-  /**
-   * Computes the differences among images. Given images image0, image1,
-   * image2, and image3, differences of the following will be computed:
-   *
-   * image0 and image1
-   * image1 and image2
-   * image2 and image3
-   */
-  diffImages() {
-    fs.readdir(this.imagesDirectoryPath, (readDirError, files) => {
-      const fullPathFiles = [];
-      if (readDirError !== null) {
-        this.emit(EVENT_ERROR_READ_IMAGES_DIR, readDirError);
+async function createTempDirectory() {
+  return new Promise((resolve, reject) => {
+    const tempDirectory = path.join(os.tmpdir(), path.sep);
+    fs.mkdtemp(tempDirectory, (error, directory) => {
+      if (error !== null) {
+        console.error('Cannot create temporary directory.');
+        reject(error);
+        return;
+      }
+      resolve(directory);
+    });
+  });
+}
+
+async function listImages(imagesDirectory) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(imagesDirectory, (error, files) => {
+      if (error !== null) {
+        const error =
+          new Error('Cannot list images in the specified directory.');
+        reject(error);
         return;
       }
       if (files.length === 0) {
-        this.emit(EVENT_ERROR_NO_IMAGES);
+        const error = new Error('No images in the specified directory.');
+        reject(error);
         return;
       }
       if (files.length === 1) {
-        this.emit(EVENT_ERROR_ONE_IMAGE);
+        const error = new Error('Only 1 images in the specified directory.');
+        reject(error);
         return;
       }
-      this.imagesDiff.on('error', (imagesDiffError) => {
-        this.emit(EVENT_ERROR_IMAGEDIFF_ERROR, imagesDiffError);
-      });
-      this.imagesDiff.on('doneAll', (differences) => {
-        console.log(util.format(
-          'Completed at %s.',
-          (new Date()).toISOString()
-        ));
-        files.forEach((currentFile, index) => {
-          let previousFile = null;
-          let difference = null;
-          let message = null;
-          if (index === 0) {
-            return;
-          }
-          difference = differences[index - 1];
-          previousFile = files[index - 1];
-          if (difference > this.differenceThreshold) {
-            message = 'WARN';
-            // Both images are considered as abnormal.
-            if (this.abnormalImages.indexOf(previousFile) < 0) {
-              this.abnormalImages.push(previousFile);
-            }
-            if (this.abnormalImages.indexOf(currentFile) < 0) {
-              this.abnormalImages.push(currentFile);
-            }
-          } else {
-            message = 'OKAY';
-          }
-          console.log(util.format(
-            '%s, %s: %s, %d',
-            previousFile,
-            currentFile,
-            message,
-            difference
-          ));
-        });
-        this.emit(EVENT_DIFF_COMPLETE);
-      });
+      const fullPathFiles = [];
       files.forEach((file) => {
-        fullPathFiles.push(this.buildImageFilePath(file));
+        fullPathFiles.push(path.join(
+          imagesDirectory,
+          path.sep,
+          file
+        ));
       });
-      console.log(util.format('Start at %s.', (new Date()).toISOString()));
-      this.imagesDiff.diffAll(fullPathFiles);
+      resolve(fullPathFiles);
     });
-  }
+  });
+}
 
-  /**
-   * Copies abnormal images to the abnormal images directory.
-   */
-  copyAbnormalImages() {
-    // Copy abnormal image files.
-    console.log(util.format('Copy start at %s.', (new Date()).toISOString()));
-    fs.mkdir(this.buildAbnormalImagesDirectoryPath(), (mkdirError) => {
-      if (mkdirError !== null) {
-        this.emit(EVENT_ERROR_CREATE_ABNORMAL_IMAGES_DIR, mkdirError);
+async function runBatchCommands(gmPath, commands) {
+  return new Promise((resolve, reject) => {
+    const gmBatchArguments = [
+      'batch',
+      '-'
+    ];
+    const gm = childProcess.spawn(gmPath, gmBatchArguments);
+    const differences = [];
+    gm.once('error', (error) => {
+      console.error('GraphicsMagick reported an error.');
+      reject(error);
+    });
+    gm.once('close', (code) => {
+      if (code !== 0) {
+        const error = new Error(`GraphicsMagick exited with code ${code}.`);
+        reject(error);
+      } else {
+        resolve(differences);
+      }
+    });
+    gm.stdout.on('data', (data) => {
+      const output = new Buffer(data, 'utf8').toString();
+      const difference = COMPARISON_RESULT_REGEX.exec(output);
+      if (difference !== null) {
+        differences.push(parseFloat(difference[1]));
+      }
+    });
+    gm.stdin.write(commands.join(''));
+    gm.stdin.end();
+  });
+}
+
+async function deleteCroppedFiles(tempDirectory) {
+  const deleteCroppedFileA = new Promise((resolve, reject) => {
+    fs.unlink(path.join(tempDirectory, CROPPED_FILE_A), (error) => {
+      if (error !== null) {
+        console.error('Cannot delete a temporary file.');
+        reject(error);
         return;
       }
-      let index = 0;
-      const copier = new Copier();
-      copier.on('done', () => {
-        if (index === (this.abnormalImages.length - 1)) {
-          console.log(util.format(
-            'Copy completed at %s.',
-            (new Date()).toISOString())
-          );
-          this.emit(EVENT_COPY_ABNORMAL_IMAGES_DONE);
-        }
-        index += 1;
-        copier.copy(
-          this.buildImageFilePath(this.abnormalImages[index]),
-          this.buildAbnormalImageFilePath(this.abnormalImages[index])
-        );
-      });
-      copier.copy(
-        this.buildImageFilePath(this.abnormalImages[index]),
-        this.buildAbnormalImageFilePath(this.abnormalImages[index])
-      );
+      resolve();
     });
+  });
+  const deleteCroppedFileB = new Promise((resolve, reject) => {
+    fs.unlink(path.join(tempDirectory, CROPPED_FILE_B), (error) => {
+      if (error !== null) {
+        console.error('Cannot delete a temporary file.');
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+  return Promise.all([
+    deleteCroppedFileA,
+    deleteCroppedFileB
+  ]);
+}
+
+async function deleteTempDirectory(directory) {
+  return new Promise((resolve, reject) => {
+    fs.rmdir(directory, (error) => {
+      if (error !== null) {
+        console.error('Cannot delete a temporary directory.');
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+async function createAbnormalImagesDirectory(directory) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(directory, (error) => {
+      if (error !== null) {
+        console.error('Cannot create abnormal images directory.');
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+async function copyAbnormalImage(source, destination) {
+  return new Promise((resolve, reject) => {
+    fs.copyFile(source, destination, (error) => {
+      if (error) {
+        console.error(`Cannot copy ${source} to ${destination}.`);
+        reject(error);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+async function init() {
+  // The 1st argument is the Node.js executable.
+  // The 2nd argument is this JavaScript file.
+  // The 3rd argument is the images directory.
+  if (process.argv.length !== 3) {
+    console.error('Usage: node index.js <images-directory>');
+    process.exit(1);
+  }
+  const imagesDirectory = process.argv[2];
+  try {
+    const config = await readConfigFile(CONFIG_FILE);
+    const tempDirectory = await createTempDirectory();
+    const images = await listImages(imagesDirectory);
+    const batchCommands = [];
+    let prevFile = null;
+    let currFile = null;
+    let prevCropFile = null;
+    let currCropFile = null;
+    // Generate a batch of commands to be executed by GraphicsMagick.
+    images.forEach((image, index) => {
+      // Swapping the previous and current cropped image files, so I don't have
+      // to crop the same image file twice.
+      if (index === 0) {
+        // Defining the name of current cropped image file is not necessary when
+        // processing the first image file, because it will be treated as
+        // previous image file.
+        prevCropFile = path.join(tempDirectory, CROPPED_FILE_B);
+      } else if ((index % 2) === 0) {
+        prevCropFile = path.join(tempDirectory, CROPPED_FILE_A);
+        currCropFile = path.join(tempDirectory, CROPPED_FILE_B);
+      } else {
+        prevCropFile = path.join(tempDirectory, CROPPED_FILE_B);
+        currCropFile = path.join(tempDirectory, CROPPED_FILE_A);
+      }
+      // Special handling of first image file, treat it as previous image file.
+      if (index === 0) {
+        prevFile = image;
+        // Crop the image only if the option is specified.
+        if (config.crop !== null) {
+          batchCommands.push(generateCropImageCommand(
+            prevFile,
+            prevCropFile,
+            config.crop
+          ));
+        }
+        return;
+      }
+      currFile = image;
+      // Crop the image only if the option is specified.
+      // This also affects which files will be used for comparison.
+      if (config.crop !== null) {
+        batchCommands.push(generateCropImageCommand(
+          currFile,
+          currCropFile,
+          config.crop
+        ));
+        batchCommands.push(generateCompareImagesCommand(
+          prevCropFile,
+          currCropFile,
+          COMPARISON_METRIC
+        ));
+      } else {
+        batchCommands.push(generateCompareImagesCommand(
+          prevFile,
+          currFile,
+          COMPARISON_METRIC
+        ));
+      }
+      prevFile = currFile;
+      prevCropFile = currCropFile;
+    });
+    console.log(`Start comparing images at ${(new Date()).toISOString()}.`);
+    const differences = await runBatchCommands(config.gmPath, batchCommands);
+    console.log(`Completed comparing images at ${(new Date()).toISOString()}.`);
+    // The number of comparisons should be the number of images minus 1.
+    const expectedComparisons = (images.length - 1);
+    if (differences.length !== expectedComparisons) {
+      console.error(`Unexpected ${differences.length} image comparisons.`);
+      process.exit(1);
+    }
+    // Clean up, delete temporary directory.
+    if (config.crop !== null) {
+      await deleteCroppedFiles(tempDirectory);
+    }
+    await deleteTempDirectory(tempDirectory);
+    const abnormalImages = new Set();
+    images.forEach((currImage, index) => {
+      if (index === 0) {
+        return;
+      }
+      const prevImage = images[index - 1];
+      const difference = differences[index - 1];
+      let message = null;
+      if (difference > config.differenceThreshold) {
+        message = 'WARN';
+        // Both images are considered as abnormal.
+        abnormalImages.add(prevImage);
+        abnormalImages.add(currImage);
+      } else {
+        message = 'OKAY';
+      }
+      console.log(`${prevImage}, ${currImage}, ${difference}: ${message}.`);
+    });
+    if (abnormalImages.size === 0) {
+      console.log('No abnormal images have been found.');
+      return;
+    }
+    if (config.abnormalImagesDirectory === null) {
+      console.log('Not copying abnormal images.');
+      return;
+    }
+    const abnormalImagesDirectory =
+      path.join(imagesDirectory, config.abnormalImagesDirectory);
+    await createAbnormalImagesDirectory(abnormalImagesDirectory);
+    // Cannot use "forEach" here because all abnormal images will be copied at
+    // once, some (slow) systems may not be able to copy over 1000 files at the
+    // same time.
+    console.log(`Start copying images at ${(new Date().toISOString())}.`);
+    for (let image of abnormalImages) {
+      await copyAbnormalImage(
+        image,
+        path.join(abnormalImagesDirectory, path.basename(image))
+      );
+    }
+    console.log(`Completed copying images at ${(new Date().toISOString())}.`);
+  } catch (error) {
+    console.error(error.message);
   }
 }
 
-/**
- * Prints program usage.
- */
-const printUsage = function printUsage() {
-  console.error('Usage: node index.js <images-directory>');
-};
-
-// Expect 3 arguments.
-if (process.argv.length !== 3) {
-  printUsage();
-  process.exit(1);
-}
-
-const application = new Application(process.argv[2]);
-application.on(EVENT_ERROR_READ_CONFIG, (error) => {
-  console.error('Unable to read configuration file.');
-  console.error(error);
-  process.exit(1);
-})
-.on(EVENT_ERROR_PARSE_CONFIG, (error) => {
-  console.error('Unable to parse configuration file.');
-  console.error(error);
-  process.exit(1);
-})
-.on(EVENT_ERROR_READ_IMAGES_DIR, (error) => {
-  console.error('Unable to read images directory.');
-  console.error(error);
-  process.exit(1);
-})
-.on(EVENT_ERROR_NO_IMAGES, () => {
-  console.log('The directory contains no images.');
-  process.exit(0);
-})
-.on(EVENT_ERROR_ONE_IMAGE, () => {
-  console.log('The directory contains 1 image file only.');
-  process.exit(0);
-})
-.on(EVENT_ERROR_IMAGEDIFF_ERROR, (error) => {
-  console.error('Unable to compare images.');
-  console.error(error);
-  process.exit(1);
-})
-.on(EVENT_ERROR_CREATE_ABNORMAL_IMAGES_DIR, (error) => {
-  console.error('Unable to create directory for abnormal images.');
-  console.error(error);
-  process.exit(1);
-})
-.on(EVENT_APPLICATION_READY, () => {
-  application.diffImages();
-})
-.on(EVENT_DIFF_COMPLETE, () => {
-  if (application.abnormalImages.length === 0) {
-    process.exit(0);
-  }
-  if (application.abnormalImagesDirectoryName === null) {
-    process.exit(0);
-  }
-  application.copyAbnormalImages();
-})
-.on(EVENT_COPY_ABNORMAL_IMAGES_DONE, () => {
-  process.exit(0);
-});
-application.parseConfig();
+init();
