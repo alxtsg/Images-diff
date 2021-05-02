f942d36c96dcb9ce8a68df85a9c29679776654c7
Alex Tsang <alextsang@live.com>
2021-05-02 21:42:28 +0900

    Test case for images comparison

--- src/tests/image-utils.test.ts
+++ src/tests/image-utils.test.ts
@@ -2,6 +2,7 @@ import assert from 'assert';
 import fs from 'fs';
 import path from 'path';
 
+import ComparisonResult from '../types/comparison-result';
 import CropConfig from '../types/crop-config';
 import * as fsUtils from '../fs-utils';
 import * as imageUtils from '../image-utils';
@@ -24,6 +25,19 @@ describe('Image utilities', async (): Promise<void> => {
       await imageUtils.cropImages(CROP_CONFIG, outputDir, files);
       const croppedFiles: string[] = await fsUtils.getFiles(outputDir);
       assert.strictEqual(croppedFiles.length, files.length);
+    });
+    await fsPromises.rmdir(outputDir, { recursive: true });
+  });
+
+  it('can compare images', async (): Promise<void> => {
+    const files: string[] = await fsUtils.getFiles(IMAGES_DIR);
+    const outputDir: string = await fsUtils.createTempDirectory();
+    await imageUtils.cropImages(CROP_CONFIG, outputDir, files);
+    await assert.doesNotReject(async (): Promise<void> => {
+      const results: ComparisonResult[] = await imageUtils.compareImages(outputDir);
+      assert.strictEqual(results.length, (files.length - 1));
+      const filteredResults: ComparisonResult[] = results.filter((results) => (results.difference > 0));
+      assert.strictEqual(filteredResults.length, 1);
     });
     await fsPromises.rmdir(outputDir, { recursive: true });
   });
