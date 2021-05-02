27725f4d1942cb64767c06009c1cb0a8f9f1403d
Alex Tsang <alextsang@live.com>
2021-05-02 21:09:44 +0900

    Filesystem utilities
    Rewrote the module in TypeScript.

--- src/tests/fs-utils.test.ts
+++ src/tests/fs-utils.test.ts
@@ -7,18 +7,22 @@ const fsPromises = fs.promises;
 
 describe('Filesystem utilities', async (): Promise<void> => {
   it('can create temporary directory', async (): Promise<void> => {
-    const directory: string = await fsUtils.createTempDirectory();
-    const status: fs.Stats = await fsPromises.stat(directory);
-    assert.strictEqual(status.isDirectory(), true);
-    await fsPromises.rmdir(directory, { recursive: true });
+    await assert.doesNotReject(async (): Promise<void> => {
+      const directory: string = await fsUtils.createTempDirectory();
+      const status: fs.Stats = await fsPromises.stat(directory);
+      assert.strictEqual(status.isDirectory(), true);
+      await fsPromises.rmdir(directory, { recursive: true });
+    });
   });
 
   it('can get absolute paths of files of a directory', async (): Promise<void> => {
     const directory = __dirname;
-    const files: string[] = await fsUtils.getFiles(directory);
-    assert.strictEqual((files.length > 0), true);
-    for (const file of files) {
-      assert.strictEqual(file.startsWith(directory), true);
-    }
+    await assert.doesNotReject(async (): Promise<void> => {
+      const files: string[] = await fsUtils.getFiles(directory);
+      assert.strictEqual((files.length > 0), true);
+      for (const file of files) {
+        assert.strictEqual(file.startsWith(directory), true);
+      }
+    });
   });
 });
